06edaed6f714626fe02a98cf4edfee1dbf933b8f
reag27@hotmail.com <reag27@hotmail.com>
2015-09-11 16:08:27 -0600

    -Deleting data folder from repository. -collect data is being kept in NSOL1 server

--- crawl_data.sh
+++ crawl_data.sh
@@ -1,4 +1,4 @@
 # Run Raul's KOSearch program in terminal
-java -cp kgml.jar KOExtraction.KOExtraction -f patric_downloads/879462.4.PATRIC.faa -o Bacteria_879462.4.PATRIC/csv/Seq_KO
+java -cp kgml.jar KOExtraction.KOextraction -f ../pathway/patric_downloads/511145.12.PATRIC.faa -o ../pathway/Bacteria_511145.12.PATRIC/csv/Seq_KO
 # Run Raul's KOSearch program in terminal
-java -cp kgml.jar KOSearch.KOSearch -i Bacteria_879462.4.PATRIC/CSV/Seq_KO.csv -o Bacteria_879462.4.PATRIC/
+#java -cp kgml.jar KOSearch.KOSearch -i Bacteria_879462.4.PATRIC/CSV/Seq_KO.csv -o Bacteria_879462.4.PATRIC/

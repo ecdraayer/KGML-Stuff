fb0c143e1809dccb3aef38607657873a5c0d2d24
reag27@hotmail.com <reag27@hotmail.com>
2015-09-13 16:43:19 -0600

    adding email functionality when an error is encountered

---
+++ src/EmailErrors/SendMail.java
@@ -0,0 +1,108 @@
+package EmailErrors;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
+/**
+ * This class will be used to send an alert email when program fails.
+ * The list of recipients will be read from maillist.lst file.
+ * @author Raul Alvarado
+ *
+ */
+public class SendMail {
+	private static BufferedReader br = null;
+	private static FileReader fileReader = null;
+	
+	
+	public SendMail () {
+		
+
+	}
+	public void send(String ProgramName, String body)
+	{
+		// TODO Auto-generated method stub
+
+	      // Sender's email ID needs to be mentioned
+	      String from = "KOExtraction@nmsu.edu";
+
+	      // Assuming you are sending email from localhost
+	      String host = "smtp.nmsu.edu";
+
+	      // Get system properties
+	      Properties properties = System.getProperties();
+
+	      // Setup mail server
+	      properties.setProperty("mail.smtp.host", host);
+	      properties.setProperty("mail.smtp.port", "587");
+	      properties.setProperty("mail.smtp.auth", "true");
+	      properties.setProperty("mail.smtp.starttls.enable", "true");
+
+	      // Get the default Session object.
+	      Session session = Session.getInstance(properties, new Authenticate());
+	
+	 
+
+	      try{
+	         // Create a default MimeMessage object.
+	         MimeMessage message = new MimeMessage(session);
+
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(from));
+
+	         // Set To: header field of the header.
+	         ArrayList <String> list = new ArrayList <String>();
+	         list= ReadEmailLst();
+	         for (String email:list)
+	        	 message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+
+	         // Set Subject: header field
+	         message.setSubject("**Program Error **" + ProgramName);
+
+	         // Now set the actual message
+	         message.setText(body);
+
+	         // Send message
+	         Transport.send(message);
+	         System.out.println("Sent message successfully....");
+	      }catch (MessagingException mex) {
+	         mex.printStackTrace();
+	      }
+	}
+	private static ArrayList<String> ReadEmailLst()	
+	{
+		ArrayList<String> list= new ArrayList<String> ();
+		try {
+			fileReader = new FileReader("maillist.lst");
+			br = new BufferedReader(fileReader);
+			String line="";
+			
+		
+			try {
+				while ((line = br.readLine()) != null) 
+				{
+					list.add(line);
+					
+					
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		return list;
+		
+	}
+
+}
--- src/KOExtraction/KOextraction.java
+++ src/KOExtraction/KOextraction.java
@@ -1,10 +1,13 @@
 package KOExtraction;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+import EmailErrors.SendMail;
 import KOSearch.ReadCSV;
 
 
@@ -24,113 +27,129 @@ public class KOextraction {
 		Integer c=0;
 		int j=0;
 		boolean contd=false; 
-		if (args.length >= 4)
+		
+		//overall try to catch general errors and send error email
+		try
 		{
-			while (j < args.length)
+			if (args.length >= 4)
 			{
-			 
-				if (args[j].equals("-f"))
-				{		
-					if (args[j+1].contains("-")==false)
-						fastaname=args[j+1];
-			
-				}
-				if (args[j].equals("-o"))
+				while (j < args.length)
 				{
-					if (args[j+1].contains("-")==false)
-						output=args[j+1] + ".csv";
-				}
-				if (args[j].equals("-r"))
-				{
-					/*option to indicate on which index to restart the program
-					if (args[j+1].contains("-")==false)
-					{
-						c=Integer.parseInt(args[j+1]);
-						contd=true;
-					}*/
-					contd=true;
-				}
-				if (args[j].equals("-k"))
-				{
-					if (args.length ==6 || args.length==8)
+				 
+					if (args[j].equals("-f"))
+					{		
+						if (args[j+1].contains("-")==false)
+							fastaname=args[j+1];
+				
+					}
+					if (args[j].equals("-o"))
 					{
 						if (args[j+1].contains("-")==false)
-							k=args[j+1];
+							output=args[j+1] + ".csv";
 					}
+					if (args[j].equals("-r"))
+					{
+						/*option to indicate on which index to restart the program
+						if (args[j+1].contains("-")==false)
+						{
+							c=Integer.parseInt(args[j+1]);
+							contd=true;
+						}*/
+						contd=true;
+					}
+					if (args[j].equals("-k"))
+					{
+						if (args.length ==6 || args.length==8)
+						{
+							if (args[j+1].contains("-")==false)
+								k=args[j+1];
+						}
+					}
+					j++;
 				}
-				j++;
-			}
-				
-		
-		
-			if (output.isEmpty()==false && fastaname.isEmpty()==false)
-			{
-		        ParseFasta fasta = new ParseFasta(fastaname);
-				BlastSearch Bsearch = new BlastSearch();
-				ExportCsv Csv = new ExportCsv(output, contd,"Sequence id,Sequence Description,gene_KO_number,organism id");
-				
-		        //determine idx to restart from
-				if (contd==true)
-				{
-				     ReadCSV readCSV = new ReadCSV(output);
-				     ArrayList<String> idxs = new ArrayList<String>();
-				     ArrayList<Integer> idxsInt = new ArrayList<Integer>();
-				     idxs = readCSV.ReadColDistinctValues(0);
-				     //convert to integer for max to work
-				     for(String s : idxs) idxsInt.add(Integer.valueOf(s));
-				     c = (Collections.max(idxsInt)) +1;
-				     //delete rows to avoid incomplete sets of results
-				     
-				   //  Csv.DeleteRows(c.toString());
-				}
-		       
-		        
-		
-		        for (int i=c; i < fasta.sequences.size() ; i++)
-		        {
-		        	//System.out.println(fasta.sequences.get(i));
-		        	Bsearch.Search(fasta.sequences.get(i),k);
-		        	
-		        	ArrayList<String> DistKOs = new  ArrayList<String> ();
-		        	DistKOs =Bsearch.ReturnDistinctValues(2);
-		        	ArrayList<String> KONums = new  ArrayList<String> ();
-		        	KONums=Bsearch.Returncolumn(2);
-		        	ArrayList<String> OrgIds = new  ArrayList<String> ();
-		        	OrgIds=Bsearch.Returncolumn(0);
-		        	
-		        	
-		        	System.out.println(i +": KO Number: " + Arrays.toString(DistKOs.toArray()) + " for " + fasta.descriptions.get(i) );
-		        	
-		        	try {
-		        		for (int i1=0 ; i1 < KONums.size();i1++)
-			        	{
-							Csv.WriteFieldCSV(Integer.toString(i), 0);
-							Csv.WriteFieldCSV(fasta.descriptions.get(i), 0);
-							Csv.WriteFieldCSV(KONums.get(i1), 0);
-							Csv.WriteFieldCSV(OrgIds.get(i1) ,0);
-							Csv.WriteFieldCSV("" ,1);
-							Csv.FlushCSV();
-							
-			        	}
-		        	
-		        		
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}	        	
-
-		        }
-		        Csv.closeCSV();
-			}
-			else
-				System.out.println("Please check parameters.");
-		}
-		else			
-		{
-			System.out.println("Please provide appropriate arguments");
-			System.out.println("-f fasta file -o output filename -k top matches(default 10) -r restart program at the last index found in csv file");
+					
 			
-		}
+			
+				if (output.isEmpty()==false && fastaname.isEmpty()==false)
+				{
+			        ParseFasta fasta = new ParseFasta(fastaname);
+					BlastSearch Bsearch = new BlastSearch();
+					ExportCsv Csv = new ExportCsv(output, contd,"Sequence id,Sequence Description,gene_KO_number,organism id");
+					
+			        //determine idx to restart from
+					if (contd==true)
+					{
+					     ReadCSV readCSV = new ReadCSV(output);
+					     ArrayList<String> idxs = new ArrayList<String>();
+					     ArrayList<Integer> idxsInt = new ArrayList<Integer>();
+					     idxs = readCSV.ReadColDistinctValues(0);
+					     //convert to integer for max to work
+					     for(String s : idxs) idxsInt.add(Integer.valueOf(s));
+					     c = (Collections.max(idxsInt)) +1;
+					     //delete rows to avoid incomplete sets of results
+					     
+					   //  Csv.DeleteRows(c.toString());
+					}
+			       
+			        
+			
+			        for (int i=c; i < fasta.sequences.size() ; i++)
+			        {
+			        	//System.out.println(fasta.sequences.get(i));
+			        	Bsearch.Search(fasta.sequences.get(i),k);
+			        	
+			        	ArrayList<String> DistKOs = new  ArrayList<String> ();
+			        	DistKOs =Bsearch.ReturnDistinctValues(20);
+			        	ArrayList<String> KONums = new  ArrayList<String> ();
+			        	KONums=Bsearch.Returncolumn(2);
+			        	ArrayList<String> OrgIds = new  ArrayList<String> ();
+			        	OrgIds=Bsearch.Returncolumn(0);
+			        	
+			        	
+			        	System.out.println(i +": KO Number: " + Arrays.toString(DistKOs.toArray()) + " for " + fasta.descriptions.get(i) );
+			        	
+			        	try {
+			        		for (int i1=0 ; i1 < KONums.size();i1++)
+				        	{
+								Csv.WriteFieldCSV(Integer.toString(i), 0);
+								Csv.WriteFieldCSV(fasta.descriptions.get(i), 0);
+								Csv.WriteFieldCSV(KONums.get(i1), 0);
+								Csv.WriteFieldCSV(OrgIds.get(i1) ,0);
+								Csv.WriteFieldCSV("" ,1);
+								Csv.FlushCSV();
+								
+				        	}
+			        	
+			        		
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}	        	
+	
+			        }
+			        Csv.closeCSV();
+			        System.out.println("Finished processing " + fastaname.toString());
+				}
+				else
+					System.out.println("Please check parameters.");
+			}
+			else			
+			{
+				System.out.println("Please provide appropriate arguments");
+				System.out.println("-f fasta file -o output filename -k top matches(default 10) -r restart program at the last index found in csv file");
+				
+			}
 		
-	}
+		}
+		catch (Exception e)
+		{
+			SendMail email= new SendMail();
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			
+			System.out.println("**Error**" + e.toString() +"\n" + sw.toString());
+			email.send("KOExtraction",sw.toString());
+		}
+}
 }

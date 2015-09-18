05bae785efa8f030189e68a9cbe197a739edb775
reag27@hotmail.com <reag27@hotmail.com>
2015-09-17 23:50:51 -0600

    crawl_data update

--- crawl_data.sh
+++ crawl_data.sh
@@ -5,10 +5,10 @@
 #java -cp kgml.jar KOExtraction.KOextraction -f ../pathway/patric_downloads/1385755.6.PATRIC.faa -o ../pathway/Bacteria_1385755.6.PATRIC/csv/Seq_KO -r
 
 # Run Raul's KOSearch program in terminal
-java -cp kgml.jar KOSearch.KOSearch -i ../pathway/Bacteria_511145.12.PATRIC/csv/Seq_KO.csv -o ../pathway/Bacteria_511145.12.PATRIC/
-java -cp kgml.jar KOSearch.KOSearch -i ../pathway/Bacteria_1385755.3.PATRIC/csv/Seq_KO.csv -o ../pathway/Bacteria_1385755.3.PATRIC/
-java -cp kgml.jar KOSearch.KOSearch -i ../pathway/Bacteria_1385755.5.PATRIC/csv/Seq_KO.csv -o ../pathway/Bacteria_1385755.5.PATRIC/
-java -cp kgml.jar KOSearch.KOSearch -i ../pathway/Bacteria_1385755.6.PATRIC/csv/Seq_KO.csv -o ../pathway/Bacteria_1385755.6.PATRIC/
+java -cp kgml.jar KOSearch.KOSearch -i ../pathway/Bacteria_511145.12.PATRIC/csv/Seq_KO.csv -o /pathway/Bacteria_511145.12.PATRIC/
+java -cp kgml.jar KOSearch.KOSearch -i ../pathway/Bacteria_1385755.3.PATRIC/csv/Seq_KO.csv -o /pathway/Bacteria_1385755.3.PATRIC/
+java -cp kgml.jar KOSearch.KOSearch -i ../pathway/Bacteria_1385755.5.PATRIC/csv/Seq_KO.csv -o /pathway/Bacteria_1385755.5.PATRIC/
+java -cp kgml.jar KOSearch.KOSearch -i ../pathway/Bacteria_1385755.6.PATRIC/csv/Seq_KO.csv -o /pathway/Bacteria_1385755.6.PATRIC/
 
 
 

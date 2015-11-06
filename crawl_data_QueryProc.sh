039961e3978b56af67d4c98b8b1f4c621eaa0463
reag27@hotmail.com <reag27@hotmail.com>
2015-11-05 17:00:55 -0700

    fix crawl.sh parameters

--- crawl_data_QueryProc.sh
+++ crawl_data_QueryProc.sh
@@ -4,5 +4,5 @@ java -cp kgml.jar TransformToGraph.TransformToGraph -c ../pathway/csv/Bacteria/ 
 java -cp kgml.jar TransformToGraph.TransformToGraph -c ../pathway/csv/Human/ -k 50 -nv
 
 # Get shortest path from GenesToQuery.csv
-java -cp kgml.jar TransformToGraph.QueryGenerator -c ../pathway/csv/Bacteria/ -k -g ../pathway/csv/Bacteria/GenesToQuery.csv -nv
-java -cp kgml.jar TransformToGraph.QueryGenerator -c ../pathway/csv/Human/ -g ../pathway/csv/Human/GenesToQuery.csv -nv
+java -cp kgml.jar TransformToGraph.QueryGenerator -f ../pathway/csv/Bacteria/ -g ../pathway/csv/Bacteria/GenesToQuery.csv -nv
+java -cp kgml.jar TransformToGraph.QueryGenerator -f ../pathway/csv/Human/ -g ../pathway/csv/Human/GenesToQuery.csv -nv
